7fe63b88522fe3c44c5109b8c3259549f92dfabe
migueljimenezjc <45280789+migueljimenezjc@users.noreply.github.com>
2019-01-31 02:49:11 -0600

    Add files via upload

---
+++ Miguel Angel Jimenez-MEX/Ejercicio1/ForgotPasswordPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Ejercicio1
+{
+    public partial class ForgotPasswordPage : ContentPage
+    {
+        public ForgotPasswordPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            this.lblRecuperar.IsVisible = false;
+            this.txtRecuperar.IsVisible = false;
+
+            this.lblCodigo.IsVisible = true;
+            this.txtCodigo.IsVisible = true;
+        }
+    }
+}
---
+++ Miguel Angel Jimenez-MEX/Ejercicio1/Login.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Ejercicio1
+{
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new ForgotPasswordPage());
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+        }
+
+        async void Handle_Clicked_1(object sender, System.EventArgs e)
+        {
+            await DisplayAlert("Aviso", "Servicio no disponible", "Cerrar");
+        }
+
+    }
+}
